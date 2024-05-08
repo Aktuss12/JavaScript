53e9a2cf53b0d0535a69f942f37126f3214434ca
Matheus Barbosa Pupim <159483547+Aktuss12@users.noreply.github.com>
2024-05-08 09:55:22 -0300

    Add files via upload

---
+++ funcoesTeste.js
@@ -0,0 +1,16 @@
+function bomDia(){
+    console.log("Bom Dia!");
+}
+bomDia();
+
+function subtracao(){
+    let subtracao = 19 - 11;
+    console.log(subtracao);
+}
+subtracao();
+
+function divisao(){
+    let divisao = 223 / 12;
+    console.log(divisao);
+}
+divisao();
