bbdac4f7179a946681c7342c0f02c3ac31387003
Matheus Barbosa Pupim <159483547+Aktuss12@users.noreply.github.com>
2024-05-10 17:16:44 -0300

    Add files via upload

---
+++ estruturaRepeticao.js
@@ -0,0 +1,13 @@
+for (contador = 0; contador < 1000; contador++){
+    console.log("Exibindo o numero" + contador);
+}
+
+for (contador = 10; contador > 0; contador--){
+    console.log("Exibindo número: " + contador)
+}
+
+var i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
